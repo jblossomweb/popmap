f4517054d614be8f17a2d651f3acefbf94a4dc4b
John Blossom <jblossom.web@gmail.com>
2015-08-17 13:44:35 -0400

    fake login, notes, minor ui changes

--- app/scripts/controllers/login.js
+++ app/scripts/controllers/login.js
@@ -10,12 +10,25 @@
 angular.module('popmap')
   .controller('LoginCtrl', function($scope, $location) {
 
+  	var fakeUser = 'admin'
+  	var fakePassword = 'password'
+
     $scope.submit = function() {
 
-    	// TODO: some login actions
-      $location.path('/dashboard')
+    	// hardcoded login for demo (normally this is a service)
+    	if($scope.user == fakeUser && $scope.password == fakePassword){
+    		$location.path('/dashboard')
+    	} else {
+    		$scope.showForgot = true
+    	}
 
       return false
+    }
+
+    $scope.demoLogin = function() {
+    	alert('Demo: "admin"/"password"')
+    	$scope.user = fakeUser
+    	$scope.password = fakePassword
     }
 
   })
